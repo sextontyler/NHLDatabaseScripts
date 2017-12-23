245bd81cbd061fa8e4321ca651aed5761999e861
mcbarlowe <mcbarlowe@gmail.com>
2017-12-23 00:30:29 -0500

    Finalized script to select top 5 players from the day before

--- top5query.py
+++ top5query.py
@@ -2,16 +2,19 @@ import psycopg2
 import pandas as pd
 import datetime
 
-date = datetime.datetime.now() - datetime.timedelta(days=1)
-print(type(date))
+top5 = 'dailytop5.txt'
+date = datetime.datetime.now() - datetime.timedelta(days = 1)
 sql_date = date.strftime('%Y-%m-%d')
-print(type(sql_date))
 conn = psycopg2.connect("host=localhost dbname=nhl user=matt")
 cur = conn.cursor()
-top5_query = "SELECT event_player_1, event_team, sum(xg) AS ixG FROM \
-        masternhlpbp WHERE game_date = '{}' GROUP BY event_player_1, event_team\
-        ORDER BY \
-        SUM(xg) DESC LIMIT 5;".format(sql_date)
-top5 = pd.read_sql(top5_query, conn)
-
-print(top5.head())
+top5_query = "SELECT event_player_1, event_team, sum(xg) AS ixG FROM\
+ masternhlpbp WHERE game_date = '{}' GROUP BY event_player_1, event_team\
+ ORDER BY SUM(xg) DESC LIMIT 5;".format(sql_date)
+print(top5_query)
+cur.execute(top5_query)
+rows = cur.fetchall()
+with open(top5, 'w+') as top5:
+    top5.write('{} ixG Leaders:\n'.format(sql_date))
+    for row in rows:
+        print(str(row))
+        top5.write('{}\n'.format(str(row)))
